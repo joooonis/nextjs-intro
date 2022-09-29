41c56051d994eba5b138c63527abfb154f095210
jooooonis <xowns3213@naver.com>
2022-09-29 22:32:13 +0900

    study : useRouter hook useRouter hook을 사용하여 현재 location에 대한 정보를 가진 router 객체를 가지고 와서 사용할 수 있습니다. Next가 기본으로 제공하는 hook으로 추가적인 라이브러리를 사용할 필요가 없습니다.

--- component/NavBar.js
+++ component/NavBar.js
@@ -1,15 +1,23 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <Link href='/'>
-        <a className='home' style={{ color: 'blue' }}>
+        <a
+          className='home'
+          style={{ color: router.pathname === '/' ? 'purple' : 'orange' }}>
           Home
         </a>
       </Link>
       <Link href='/about'>
-        <a>About</a>
+        <a
+          style={{ color: router.pathname === '/about' ? 'purple' : 'orange' }}>
+          About
+        </a>
       </Link>
     </div>
   );

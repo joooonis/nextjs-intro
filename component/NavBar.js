638e9eced858fdc2faccd4e6289e966b206f7803
jooooonis <xowns3213@naver.com>
2022-09-29 22:20:15 +0900

    study : Link component Link component에는 href 속성만 줄 수 있고 아래 anchor tag들에 속성들을 대신 입력합니다.

--- component/NavBar.js
+++ component/NavBar.js
@@ -4,7 +4,9 @@ export default function NavBar() {
   return (
     <div>
       <Link href='/'>
-        <a>Home</a>
+        <a className='home' style={{ color: 'blue' }}>
+          Home
+        </a>
       </Link>
       <Link href='/about'>
         <a>About</a>

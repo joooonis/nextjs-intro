2e177131c1abe7a5262144c1208070e500fb2f7b
jooooonis <xowns3213@naver.com>
2022-10-01 02:10:14 +0900

    study : Layout component Next에서는 Layout component를 만들어 기본 레이아웃으로 활용합니다. nav, footer 등을 보통 이 component에 위치시킵니다.

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,10 @@
-import NavBar from '../component/NavBar';
+import Layout from '../component/LayOut';
 import '../styles/globals.css'; // _app.js에서 밖에 import 할 수 없습니다.
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <span>수면 아래에서 일어나고 있움...</span>
-      <style jsx global>{`
-        h1 {
-          color: forestgreen;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }

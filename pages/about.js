eb3def394f8ba972738ba889acdce9074bdc9012
jooooonis <xowns3213@naver.com>
2022-10-01 02:23:14 +0900

    study : Head component Next에서 제공하는 Head component를 사용하여 page마다 title을 지정해줍니다. Seo component를 만들어서 좀 더 사용성이 좋게 만듭니다.

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Seo from '../component/Seo';
+
 export default function About() {
   return (
     <div>
+      <Seo title='About' />
       <h1>Here is About</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
+import Head from 'next/head';
+import Seo from '../component/Seo';
+
 export default function Home() {
   return (
     <div>
+      <Seo title='Home' />
       <h1 className='active'>Home</h1>
-      <div>
-        <h2 className='active'>Home</h2>
-
-        <style jsx>{`
-          nav {
-            background-color: forestgreen;
-          }
-
-          .active {
-            color: hotpink;
-          }
-        `}</style>
-      </div>
     </div>
   );
 }

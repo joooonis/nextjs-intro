783c6f8d08d71ca1c80ae6be50f84d6ba505df85
jooooonis <xowns3213@naver.com>
2022-10-01 03:03:02 +0900

    feat : fetch movies fetch data using The Movie Database API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Seo from '../component/Seo';
 
+const API_KEY = '51a50e1e798cc339aa2a5af32624c0f9';
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+      );
+      const { results } = await response.json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title='Home' />
-      <h1 className='active'>Home</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }

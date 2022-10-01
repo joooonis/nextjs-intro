59495ab3032c36a662da97ee3cde4ddc2567e81f
jooooonis <xowns3213@naver.com>
2022-10-02 01:34:47 +0900

    study : rewrites rewirtes를 사용하면 들어오는 request 경로를 다른 destination 결로로 매핑할 수 있습니다. 사용자는 실제 경로를 알 수 없습니다.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = '51a50e1e798cc339aa2a5af32624c0f9';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -8,6 +11,14 @@ const nextConfig = {
         source: '/old-blog/:path*',
         destination: '/new-blog/:path*',
         permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
       },
     ];
   },

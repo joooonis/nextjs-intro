9f96b723c7bae725818ddac8f2b4a9aa1ee29fbb
jooooonis <xowns3213@naver.com>
2022-10-03 00:14:54 +0900

    design : css styling

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,41 @@ export default function Home({ results }) {
   const [movies, setMovies] = useState();
 
   return (
-    <div>
+    <div className='container'>
       <Seo title='Home' />
       {results?.map((movie) => (
-        <div key={movie.id}>
+        <div className='movie' key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+
+        .movie {
+          cursor: pointer;
+        }
+
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }

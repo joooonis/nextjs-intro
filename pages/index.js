a285a96da367e85cebc95256a094c69397004ef0
jooooonis <xowns3213@naver.com>
2022-10-03 00:52:06 +0900

    study : routing with queries router.push()를 할 때 query를 함께 담아서 routing 할 수 있고 as option을 가지고 url을 masking 할 수 있습니다.

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,36 @@ import Seo from '../component/Seo';
 
 export default function Home({ results }) {
   const router = useRouter();
-  const onClick = (id) => {
-    router.push(`/movies/${id}`);
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          id,
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
   };
 
   return (
     <div className='container'>
       <Seo title='Home' />
       {results?.map((movie) => (
-        <div key={movie.id} className='movie'>
-          <img
-            onClick={() => onClick(movie.id)}
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          />
-          <Link href={`/movies/${movie.id}`}>
+        <div
+          key={movie.id}
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className='movie'>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <Link
+            href={{
+              pathname: `/movies/${movie.id}`,
+              query: {
+                title: movie.original_title,
+              },
+            }}
+            as={`/movies/${movie.id}`}>
             <a>
               <h4>{movie.original_title}</h4>
             </a>
